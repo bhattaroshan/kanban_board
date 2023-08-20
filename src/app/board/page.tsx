83802a64266a09eea553bf0e07877cf4a13758c6
roshanbhatta <bhattaroshan@gmail.com>
2023-08-20 15:44:19 +0545

    experimented with all blocks

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -40,7 +40,9 @@ function page() {
         },
         {
             name: 'Done',
-            items: []
+            items: [
+                {task: 'Take people to retreat', id:'105'}
+            ]
         },
     ]);
 
@@ -64,15 +66,23 @@ function page() {
 
             //same column, handle only shuffles
             if(destination.droppableId===source.droppableId){
-                let temp = [...board];
-                let temp2 = temp[parseInt(destination.droppableId)].items;
-                const removed = temp2[source.index];
-                temp2.splice(source.index,1);
-                temp2.splice(destination.index,0,removed);
+                let temp = [...board][parseInt(destination.droppableId)].items;
+                const removed = temp[source.index];
+                temp.splice(source.index,1);
+                temp.splice(destination.index,0,removed);
                 let temp3 = [...board];
-                temp3[parseInt(destination.droppableId)].items=temp2;
-                console.log(temp3)
+                temp3[parseInt(destination.droppableId)].items=temp;
                 setBoard(temp3);
+            }else{ //different column
+                let tempBoard = Array.from(board);
+                let sourceColumn = tempBoard[parseInt(source.droppableId)];
+                let destinationColumn = tempBoard[parseInt(destination.droppableId)];
+                const [removedSource] = sourceColumn.items.splice(source.index,1);
+                destinationColumn.items.splice(
+                     destination.index,0,removedSource);
+                
+                setBoard(tempBoard);
+                
             }
 
         }
@@ -85,7 +95,7 @@ function page() {
             <Droppable droppableId='board' direction='horizontal' type='column'>
                 {(provided)=>(
                     <div {...provided.droppableProps} ref={provided.innerRef}
-                        className='grid grid-cols-1 sm:grid-cols-3 gap-5 m-4'
+                        className='grid grid-cols-1 sm:grid-cols-3 gap-5 m-4 max-w-4xl mx-auto'
                     >
                         {
                             board.map((column,index)=>(
@@ -97,14 +107,13 @@ function page() {
                                             <Droppable droppableId={index.toString()}
                                                 type='card'>
                                                     {(provided,snapshot)=>(
-                                                       <div className={`p-2 bg-red-400 rounded-md font-bold
-                                                                    ${snapshot.isDraggingOver?'bg-green-300':'bg-red-300'}`}
+                                                       <div className={`p-2 bg-red-400 rounded-md font-bold ${snapshot.isDraggingOver&&'bg-blue-300'}`}
                                                        {...provided.droppableProps} ref={provided.innerRef}>
                                                         <h1 className='text-white'>{column.name}</h1>
 
                                                         {
                                                             column.items.map((item,idx)=>(
-                                                                <Draggable key={index+idx} draggableId={(index*10+idx).toString()} index={idx}>
+                                                                <Draggable key={item.id} draggableId={item.id} index={idx}>
                                                                     {(provided)=>(
                                                                         <div 
                                                                         {...provided.dragHandleProps}
